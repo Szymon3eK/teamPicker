59c1922543124dba1daf44650f4c16f74d1184fa
Szymon Hunek <sz.hunek.sh@gmail.com>
2024-01-31 23:05:31 +0100

    delete comma

--- functions/createChannel.js
+++ functions/createChannel.js
@@ -34,7 +34,7 @@ module.exports = async (team1, team2, client, interaction, userChannel, slotsPer
 
 
     const startembed = new EmbedBuilder()
-    .setDescription(`Podzielono wszystkich na kanaly! **GL HF** 🥰 \n *${config.src}`)
+    .setDescription(`Podzielono wszystkich na kanaly! **GL HF** 🥰 \n ${config.src}`)
     .setColor('#37eb34')
     .setTimestamp();
 
